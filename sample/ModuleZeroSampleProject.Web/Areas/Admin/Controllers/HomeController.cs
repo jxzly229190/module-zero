0d65327f33d50f708a60e78104fc677158e9721e
JimmyZhang <jx-zly@live.cn>
2015-07-01 23:28:31 +0800

    重构后台

---
+++ sample/ModuleZeroSampleProject.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Abp.Web.Mvc.Authorization;
+
+namespace ModuleZeroSampleProject.Web.Areas.Admin.Controllers
+{
+    public class HomeController : ModuleZeroSampleProjectControllerBase
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(); //Layout of the angular application.
+        }
+    }
+}
---
+++ sample/ModuleZeroSampleProject.Web/Areas/Admin/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModuleZeroSampleProject.Web.Areas.Admin
+{
+    public class DefaultController : Controller
+    {
+        // GET: Admin/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
