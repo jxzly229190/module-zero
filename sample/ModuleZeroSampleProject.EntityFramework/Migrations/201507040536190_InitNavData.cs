487e20747435e243a127e20b6cd932a307454f7b
JimmyZhang <jx-zly@live.cn>
2015-07-05 22:37:45 +0800

    update

---
+++ sample/ModuleZeroSampleProject.EntityFramework/Migrations/201507040536190_InitNavData.cs
@@ -0,0 +1,18 @@
+namespace ModuleZeroSampleProject.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure.Annotations;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitNavData : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
